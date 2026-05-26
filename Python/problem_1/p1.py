2c639aa459a8e318103e6b5b2eeac612c7caa653
djorgeext <djorge.@fi.uba.ar>
2026-05-26 16:57:58 -0300

    first commit

--- Python/problem_1/p1.py
+++ Python/problem_1/p1.py
@@ -3,4 +3,15 @@ with open("Inputs/input", "r") as f:
 
 rotations = [x[0] for x in lines]
 degrees = [int(x[1:]) for x in lines]
-print(degrees)
+
+count = 0
+position = 50
+
+for i in range(len(rotations)):
+    if rotations[i] == "R":
+        temp = position + degrees[i]
+        if temp >= 100:
+            temp -= 100
+        position = temp
+
+    else:
